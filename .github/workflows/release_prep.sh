3c8ed6675a679216e55aca9a77331b9398b5b804
Alexandre Martani <amartani@gmail.com>
2026-04-24 14:43:18 +0000

    chore: Rename from rules_mylang to rules_mise for mise tool runner

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -8,8 +8,8 @@ TAG=$1
 # The prefix is chosen to match what GitHub generates for source archives
 # This guarantees that users can easily switch from a released artifact to a source archive
 # with minimal differences in their code (e.g. strip_prefix remains the same)
-PREFIX="rules_mylang-${TAG:1}"
-ARCHIVE="rules_mylang-$TAG.tar.gz"
+PREFIX="rules_mise-${TAG:1}"
+ARCHIVE="rules_mise-$TAG.tar.gz"
 
 # NB: configuration for 'git archive' is in /.gitattributes
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
@@ -30,7 +30,7 @@ cat << EOF
 2. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(name = "com_myorg_rules_mylang", version = "${TAG:1}")
+bazel_dep(name = "com_rules_mise_rules_mise", version = "${TAG:1}")
 \`\`\`
 
 ## Using WORKSPACE
@@ -40,10 +40,10 @@ Paste this snippet into your \`WORKSPACE.bazel\` file:
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
-    name = "com_myorg_rules_mylang",
+    name = "com_rules_mise_rules_mise",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/myorg/rules_mylang/releases/download/${TAG}/${ARCHIVE}",
+    url = "https://github.com/rules_mise/rules_mise/releases/download/${TAG}/${ARCHIVE}",
 )
 EOF
 
